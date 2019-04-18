1d6cdc99053bc4291127e29eef4027c255ecbdca
DX2DigitalGroup <dbuihien@my.bcit.ca>
2019-04-17 20:11:40 -0700

    v1.2 - updated preview section

--- CLISearch/GoogleSearch/GoogleLogo-config.sh
+++ CLISearch/GoogleSearch/GoogleLogo-config.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
+function drawlinea(){
+	sw=$COLUMNS
+	cuff=$(expr "$(expr "$COLUMNS" \- "$(echo "Previewq" | wc -L)")" \/ 2)
+	while [ $sw -gt 0 ]; do
+		echo -en "\033[30;43m \033[m";
+		((sw -=1));
+	done
+	echo -en "\r"; tput cuf $cuff; echo -e "\033[30;43mPreview\033[m"
+	unset screenwidth cuff
+}
+function drawlineb(){
+	screenwidth=$(tput cols)
+	cuff=$(expr "$(expr "$COLUMNS" \- "$(echo "Preview - END" | wc -L)")" \/ 2)
+	while [ "$sceenwidth" -gt 0 ]; do
+		echo -en "\033[43m \033[m"
+		((screenwidth -=1))
+	done
+	echo -en "\r"; tput cuf $cuff; echo -e "\033[43mPreview - END\033[m"
+	unset screenwidth cuff
+}
 
 function getsize(){
+	clear
 	echo -e """
 Please select a logo size to use:
-(Note: 	Use the LOWERCASE letter corresponding to
-		your select to make your selection.
 
-		Use UPPERCASE to PREVIEW your selection.)
+(Note:	Use the LOWERCASE letter corresponding to
+	your select to make your selection.
+
+	Use UPPERCASE to PREVIEW your selection.)
 
 	l/L) Large
 	m/M) Medium
@@ -36,7 +58,8 @@ Please select a logo size to use:
 		;;
 	L)
 		clear
-		echo -e "\n\n\033[04;37m                              PREVIEW - BEG                              \n \033[m"
+		drawlinea
+#		echo -e "\n\n\033[07;33m                              PREVIEW - BEG                              \033[m"
 		echo -e """ 
 
 
@@ -58,12 +81,14 @@ Please select a logo size to use:
 """
 
 		echo -e -n "\e[00;00m    \e[30;47m                                                           \e[00;00m \e[07;01;37;40m Google Search \e[00;00m\n\n  HELP:\n\t[\033[01;32mArrows\033[m]: Move Cursor\t\t[\033[01;32mq\033[m]: Quit\t[\033[01;32mU\033[m]\tOpen URL\n\t[\033[01;32mReturn\033[m]: Enter / Left Click\t[\033[01;32mB\033[m]: Back\t[\033[01;32mH\033[m]\tBrowser Help\n\n\n"
-		echo -e "\033[04;37m                              PREVIEW - END                              \033[m\n\n"
+		drawlinea
+#		echo -e "\033[07;33m                              PREVIEW - END                              \033[m\n\n"
 		read -p '[ Press any key to continue ]' -n 1
 		getsize
 		;;
 	M)
 		clear
+		drawlinea
 		echo -e "\033[0m"
 		echo -e '''
 
@@ -75,22 +100,30 @@ Please select a logo size to use:
 	               [01;34m|___/[m
 
    [30;47m                                       [m  [30;47m Google Search [m'''
-	   echo -en "\033[m\n\n"
+		echo -e "\n\n\n"
+		drawlinea
+	   	echo -en "\033[m\n\n"
 		read -p '[ Press any key to continue ]' -n 1
 		getsize
 		;;
 	S)
 		clear
+		drawlinea
 		echo -e "\033[0m"
 		echo " "					# Blank line
 		echo -e "\n\n\e[m\t   \e[01;34;40mG\e[01;31;40mo\e[33;40mo\e[34;40mg\e[32;40ml\e[31;40me${cCLR}";echo ' ';printf "\e[m  \e[01;30;47m                          \e[00m \e[30;47m Search \e[00;00m\n\n\n "
+		echo -e "\n"
+		drawlinea
 		read -p '[ Press any key to continue ]' -n 1
 		getsize
 		;;
 	O)
 		clear
+		drawlinea
 		echo " "					# Blank line
 		echo -e "[01;34;40mG[01;31;40mo[33;40mo[34;40mg[32;40ml[31;40me [01;37mSearch \n:\n\n "
+		echo -e "\n"
+		drawlinea
 		read -p '[ Press any key to continue ]' -n 1
 		getsize
 		;;
