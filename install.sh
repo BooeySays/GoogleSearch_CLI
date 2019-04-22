d23ff3a82f745dc522bb101fd0dc0313aab51c08
DX2DigitalGroup <dbuihien@my.bcit.ca>
2019-04-21 21:34:39 -0700

    added check for w3m

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if ! hash w3m 2>/dev/null/; then
+	if hash pkg 2>/dev/null/; then
+		pkg update -y && pkg install w3m -y
+	elif hash apt 2>/dev/null/; then
+		apt update -y && apt install w3m -y
+	fi
+fi
+
 if [ $BOOEYSFILES ]; then
 	cp -r ./CLISearch $BOOEYSFILES
 fi
